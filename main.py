0e524bc5c7f28e5d0b48a2900bf28968cc7a0bf2
Artur T Małas <artur.malas25@gmail.com>
2023-11-18 12:07:23 +0100

    Add functionality to calculate m1 and c1

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QComboBox
 from PyQt6.QtCore import QSize, Qt
+import os
+
+import PyQt6
 
 import sys
+
+#os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'/Users/artur/opt/anaconda3/lib/python3.9/site-packages/PyQt6/Qt6/plugins'
+
 
 calculate_str = "Oblicz"
 
@@ -20,28 +26,40 @@ class MainWindowLayout(QWidget):
         super().__init__()
 
         # Buttons
-        calculate_btn = QPushButton(calculate_str)
+        calculate_btn = QPushButton(calculate_str, self)
+        calculate_btn.clicked.connect(self.calculate_missing_variable)
 
         # Input fields and labels
         m1_label = QLabel("m1", self)
         m1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.m1_input = QLineEdit(self)
         c1_label = QLabel("c1", self)
+        c1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.c1_input = QLineEdit(self)
         delta_T1_label = QLabel("delta_T1", self)
+        delta_T1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.delta_T1_input = QLineEdit(self)
 
         m2_label = QLabel("m2", self)
+        m2_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.m2_input = QLineEdit(self)
         c2_label = QLabel("c2", self)
+        c2_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.c2_input = QLineEdit(self)
         delta_T2_label = QLabel("delta_T2", self)
+        delta_T2_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.delta_T2_input = QLineEdit(self)
+
+        self.missing_variable_text = "Result"
+        self.missing_variable_value = str(0.0)
+
+        self.result_label = QLabel(self.missing_variable_text, self)
+        self.result_value_label = QLabel(self.missing_variable_value, self)
 
         # Drop-down list
         self.chosen_variable_combobox = QComboBox(self)
         self.chosen_variable_combobox.currentTextChanged.connect(self.choose_variable)
-        list_of_variables = ["m1", "c1", "delta T1", "m2", "c2", "delta T2"]
+        list_of_variables = ["m1", "c1"]
         for variable in list_of_variables:
             self.chosen_variable_combobox.addItem(variable)
         chosen_variable_label = QLabel("Wybierz szukaną niewiadomą")
@@ -90,6 +108,13 @@ class MainWindowLayout(QWidget):
         
         main_layout.addLayout(layout)
 
+        main_layout.addWidget(calculate_btn)
+
+        result_layout = QHBoxLayout(self)
+        result_layout.addWidget(self.result_label)
+        result_layout.addWidget(self.result_value_label)
+        main_layout.addLayout(result_layout)
+
         self.setLayout(main_layout)
 
     def choose_variable(self):
@@ -97,14 +122,30 @@ class MainWindowLayout(QWidget):
 
         if chosen_variable == "m1":
             self.m1_input.setEnabled(False)
+            self.m1_input.setText("")
             self.c1_input.setEnabled(True)
-            if self.m2_input.text() != "" and self.c2_input.text() != "" and self.delta_T2_input.text() != "" and self.c1_input.text() != "" and self.delta_T1_input.text() != "":
-                m1_value = str((float(self.m2_input.text()) * float(self.c2_input.text()) * float(self.delta_T2_input.text())) / (float(self.c1_input.text()) * float(self.delta_T1_input.text())))
-                self.m1_input.setText(m1_value)
+            self.result_label.setText("m1")
+
+            
         elif chosen_variable == "c1":
             self.c1_input.setEnabled(False)
+            self.c1_input.setText("")
             self.m1_input.setEnabled(True)
+            self.result_label.setText("c1")
 
+
+    def calculate_missing_variable(self):
+        if self.m2_input.text() != "" and self.c2_input.text() != "" and self.delta_T2_input.text() != "" and self.c1_input.text() != "" and self.delta_T1_input.text() != "":
+            m1_value = str(round((float(self.m2_input.text()) * float(self.c2_input.text()) * float(self.delta_T2_input.text())) / (float(self.c1_input.text()) * float(self.delta_T1_input.text())), 5))
+            self.m1_input.setText(m1_value)
+            self.result_value_label.setText(m1_value)
+            print("m1")
+        
+        elif self.m2_input.text() != "" and self.c2_input.text() != "" and self.delta_T2_input.text() != "" and self.m1_input.text() != "" and self.delta_T1_input.text() != "":
+            c1_value = str(round((float(self.m2_input.text()) * float(self.c2_input.text()) * float(self.delta_T2_input.text())) / (float(self.m1_input.text()) * float(self.delta_T1_input.text())), 5))
+            self.c1_input.setText(c1_value)
+            self.result_value_label.setText(c1_value)
+            print("c1")
             
 
 
